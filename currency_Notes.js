bdc3d638a860078de42f39be70070ae1961c82ea
navyaan123 <92245202+navyaan123@users.noreply.github.com>
2023-11-16 20:56:02 +0500

    Add files via upload

--- currency_Notes.js
+++ currency_Notes.js
@@ -5,28 +5,27 @@
 // withdrawer.
 
 var num1,num2,num3;
-var array = [];
+var arrayNotes = [];
 
 var amount = Number(prompt("Enter amount  which is divisible by 10"));
 
-
-
-if (!isNaN(amount)) {
+if (isNaN(amount)) {
     
-        if (amount %10==0) {
+    alert("Please enter a number which is divisible by 10.");
     
-            array=findNotes(amount);         
-            alert(array[0]+" hundred Notes  "+array[1]+" fifty Notes  "+array[2]+" ten Notes ")
-            
-        }
-        else {
-            
-            alert("you entered a number that is not divisible by 10")
-        }
 }
-else {
-        alert("Please enter a number which is divisible by 10.");
-    }
+else if (amount %10==0) {
+    
+    findNotes(amount);
+    alert(arrayNotes[0]+arrayNotes[1]+arrayNotes[2]+arrayNotes[3]+arrayNotes[4]+arrayNotes[5]);
+}
+else
+{
+            
+alert("you entered a number that is not divisible by 10")
+}
+
+    
 
             
 
@@ -35,8 +34,27 @@ function findNotes(amount) {
     num1 = Math.floor(amount / 100);
     num2 = Math.floor((amount % 100) / 50);
     num3 = ((amount % 100 % 50) / 10);
-
-    array.push(num1, num2, num3)
-    return array;
- }   
     
+    arrayNotes.push(num1," hundred Notes ",num2," fifty Notes ",num3," ten Notes");
+    
+    
+    if (arrayNotes[0] == 0)
+    {
+        arrayNotes[0] = "";
+        arrayNotes[1] = "";
+    }
+    
+    if (arrayNotes[2] == 0)
+    {
+        arrayNotes[2] = "";
+        arrayNotes[3] = "";
+    }
+
+    if (arrayNotes[4] == 0)
+    {
+        arrayNotes[4] = "";
+        arrayNotes[5] = "";
+    }
+}
+
+
